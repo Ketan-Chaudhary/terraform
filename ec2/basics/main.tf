1275af895433f92ef8c50752d9b16be8a42ed693
Ketan-Chaudhary <ketanchaudharypoonia@gmail.com>
2025-06-27 02:45:52 +0530

    SSH issue

--- ec2/basics/main.tf
+++ ec2/basics/main.tf
@@ -3,9 +3,38 @@ resource "aws_instance" "ec2_main" {
   instance_type = var.instance_type
   key_name = var.key_name
   associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.ssh_access.id]
 
   tags = {
     Name = "ec2-terraform"
     Environment = "test"
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "ssh_access" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.aws_vpc.default.id 
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh-access"
   }
 }
